1d9bee5628382a76aa58f49479add3a6ecafc4d5
Haram\Administrator <kieav98@gmail.com>
2024-02-17 02:54:11 +0900

    MinHourlyWageCalculator is updated on Feb 17th, 2024.
    I added "while" to prevent the choice of numbers other than 1 and 2.

--- new_proj/new_proj/MinHourlyWageCalculator.cpp
+++ new_proj/new_proj/MinHourlyWageCalculator.cpp
@@ -1,18 +1,26 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+/*Updated on February 17, 2024.*/
 int main() {
 	int new_wage = 9860;
 	int old_wage = 9620;
 	int which_wage, hourly_wage, time, income;
 	printf("시급을 월급으로 계산해주는 프로그램입니다.\n");
-	printf("신규 최저임금인지, 기존 최저임금인지 입력하시오.\n");
-	printf("예) 신규 = 1, 혹은 기존 = 2\n");
-	scanf("%d", &which_wage);
+
+	while (1) {
+		printf("숫자로 어떤 최저임금으로 할 것인지 선택하시오.\n");
+		printf("1: 신규 최저임금\n2: 기존 최저임금\n");
+		scanf("%d", &which_wage);
+		if (which_wage == 1 || which_wage == 2) {
+			break;
+		} else {
+			printf("1과 2만 선택할 수 있습니다.\n다시 선택하시오.\n");
+		}
+	}
 
 	if (which_wage == 1) {
 		hourly_wage = new_wage;
-	}
-	else {
+	} else {
 		hourly_wage = old_wage;
 	}
 
